9ff19b1821300f5e72d2c1a980c7dd8ca0c75988
craig.matthew.smith@gmail.com <craig.matthew.smith@gmail.com>
2020-10-26 10:41:25 -0700

    dfs practice

---
+++ tree_dfs_oop.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 26 10:26:51 2020
+
+@author: csmith
+"""
+import collections
+
+class TreeNode:
+    def __init__(self, val=None, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+class Solution:
+    
+    # pattern 3 - works
+    # def findMode(self, root):
+    #     count = collections.defaultdict(int)
+    #     self.dfs(root, count)
+    #     print(count)
+    #     max_count = max(count.values())
+    #     print(max_count)
+    #     result = [key for key, value in count.items() if value == max_count]
+    #     return result
+    # def dfs(self, node, count):
+    #     if not node:
+    #         return
+    #     count[node.val] += 1
+    #     self.dfs(node.left, count)
+    #     self.dfs(node.right, count)
+
+
+    def findMode(self, root):
+        def dfs(node, count):
+            if not node:
+                return
+            count[node.val] += 1
+            dfs(node.left, count)
+            dfs(node.right, count)
+        count = collections.defaultdict(int)
+        dfs(root, count)
+        print(count)
+        max_count = max(count.values())
+        print(max_count)
+        result = [key for key, value in count.items() if value == max_count]
+        return result
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(4)
+root.right.left = TreeNode(4)
+root.right.right = TreeNode(4)
+
+
+answer = Solution()
+print(answer.findMode(root))        
